207556ddb50b8bf97e6d0d898e637abec3a8c76f
Prabhjot Singh <35163500+resurrected21@users.noreply.github.com>
2018-12-02 15:26:04 +0530

    add:build script

---
+++ AnyKernel2/anykernel.sh
@@ -0,0 +1,38 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=Prototype by @res21
+do.devicecheck=0
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=YUNIQUE
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel permissions
+# set permissions for included ramdisk files
+chmod -R 755 $ramdisk
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+
+## end install
